bdd53c80f855ff523627e76a71f0c49e9d3fcd4f
Claudio Vitor <claudiovitor2001@gmail.com>
2022-01-18 14:26:20 -0300

    entry pages implemented

---
+++ wiki/encyclopedia/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    content = util.get_entry(title)
+
+    if content == None:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title, "content": "A página solicitada não foi encontrada."
+        })
+
+    return render(request, "encyclopedia/entry.html", {
+        "title": title, "content": content
+    })
