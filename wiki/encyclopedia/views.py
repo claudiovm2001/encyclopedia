1928715b953d132a35675030196beae56c0b74dd
Claudio Vitor <claudiovitor2001@gmail.com>
2022-01-21 13:25:05 -0300

    search bar render fix

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -22,39 +22,41 @@ def index(request):
 
 def entry(request, title):
     content = util.get_entry(title)
+    form = NewSearchForm()
 
     if content == None:
         return render(request, "encyclopedia/entry.html", {
-            "title": title, "content": "A página solicitada não foi encontrada."
+            "title": title, "content": "A página solicitada não foi encontrada.", "form_search": form
         })
 
     return render(request, "encyclopedia/entry.html", {
-        "title": title, "content": content
+        "title": title, "content": content, "form_search": form
     })
 
 def create(request):
+    form_search = NewSearchForm()
 
     if request.method == "POST":
         form = NewEntryForm(request.POST)
-
+        
         if form.is_valid():
 
             title = form.cleaned_data["title"]
 
             if util.get_entry(title) != None:
                 return render(request, "encyclopedia/entry.html", {
-                    "title": title, "content": "Esta página já existe."
+                    "title": title, "content": "Esta página já existe.", "form_search": form_search
                 })
 
             content = form.cleaned_data["content"]
             util.save_entry(title, content)
 
             return render(request, "encyclopedia/entry.html", {
-                "title": title, "content": content
+                "title": title, "content": content, "form_search": form_search
             })
 
     return render(request, "encyclopedia/create.html", {
-        "form_create": NewEntryForm()
+        "form_create": NewEntryForm(), "form_search": form_search
     })
 
 def search(request):
@@ -77,7 +79,7 @@ def search(request):
             results = results_low + results_up
 
             return render(request, "encyclopedia/index.html", {
-                "entries": results, "text": "Resultados: "
+                "entries": results, "text": "Resultados: ", "form_search": NewSearchForm()
             })
 
 def edit(request, title):
@@ -95,7 +97,7 @@ def edit(request, title):
             util.delete_entry(title_old)
 
             return render(request, "encyclopedia/entry.html", {
-                "title": title, "content": content
+                "title": title, "content": content, "form_search": NewSearchForm()
             })
 
     form = NewEntryForm(initial={
@@ -103,7 +105,7 @@ def edit(request, title):
     })
 
     return render(request, "encyclopedia/edit.html", {
-        "form_create": form, "title": title
+        "form_create": form, "title": title, "form_search": NewSearchForm()
     })
 
 def randomize(request):
