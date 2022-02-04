62cad46d70da1450bc6ec4d751670d6546fa87c0
Cláudio Vitor Mendonça <57025570+claudiovm2001@users.noreply.github.com>
2022-02-03 22:11:03 -0300

    Add comments(2)
    General comments + regarding ownership

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -4,6 +4,8 @@ from . import util
 
 from django import forms
 from django.http import HttpResponseRedirect
+
+#Código autoral abaixo:
 import random 
 
 class NewEntryForm(forms.Form):
@@ -20,10 +22,12 @@ def index(request):
         "entries": util.list_entries(), "form_search": form, "text": "All Pages"
     })
 
+#View para cada entrada da enciclopédia:
 def entry(request, title):
     content = util.get_entry(title)
     form = NewSearchForm()
-
+    
+    #Se a página não existir nos arquivos:
     if content == None:
         return render(request, "encyclopedia/entry.html", {
             "title": title, "content": "A página solicitada não foi encontrada.", "form_search": form
@@ -33,6 +37,7 @@ def entry(request, title):
         "title": title, "content": content, "form_search": form
     })
 
+#View para criar uma página nova:
 def create(request):
     form_search = NewSearchForm()
 
@@ -42,7 +47,8 @@ def create(request):
         if form.is_valid():
 
             title = form.cleaned_data["title"]
-
+    
+            #Se uma página com o mesmo título já existir:
             if util.get_entry(title) != None:
                 return render(request, "encyclopedia/entry.html", {
                     "title": title, "content": "Esta página já existe.", "form_search": form_search
@@ -59,6 +65,7 @@ def create(request):
         "form_create": NewEntryForm(), "form_search": form_search
     })
 
+#View para pesquisa:
 def search(request):
 
     if request.method == "POST":
@@ -67,32 +74,37 @@ def search(request):
         if form.is_valid():
 
             query = form.cleaned_data["query"]
-
+            
+            #Se o título pesquisado for EXATAMENTE igual a algum já presente nos arquivos:
             if util.get_entry(query) != None:
-                return HttpResponseRedirect("wiki/"+query)
+                return HttpResponseRedirect("wiki/"+query) #Ir para a página com este título
 
             l = util.list_entries()
-
+            
+            #Se o começo do texto pesquisado corresponder ao que existe nos arquivos:
             results_low = list(filter(lambda x: x.startswith(query.lower()), l))
             results_up = list(filter(lambda x: x.startswith(query.upper()), l))
 
-            results = results_low + results_up
+            results = results_low + results_up #Desconsiderar caixa alta/baixa para o título
 
             return render(request, "encyclopedia/index.html", {
                 "entries": results, "text": "Resultados: ", "form_search": NewSearchForm()
             })
 
+#View para edição de página:
 def edit(request, title):
 
     if request.method == "POST":
         form = NewEntryForm(request.POST)
 
         if form.is_valid():
-
+            
+            #Faz a troca do título antigo da página pelo novo:
             title_old = title
             title = form.cleaned_data["title"]
             content = form.cleaned_data["content"]
-
+            
+            #Salva a nova página e apaga a antiga:
             util.save_entry(title, content)
             util.delete_entry(title_old)
 
@@ -108,9 +120,13 @@ def edit(request, title):
         "form_create": form, "title": title, "form_search": NewSearchForm()
     })
 
+#Função para página aleatória:
 def randomize(request):
     l = util.list_entries()
     num = random.randint(0,(len(l)-1))
     target = l[num]
+    
+    #Seleciona um número aleatório, com valores entre 0 e o total de páginas,
+    #depois usa esse valor para redirecionar
 
     return HttpResponseRedirect("wiki/"+target)
