876a81f008625b4f070b4189166880a5d03f44cc
Cláudio Vitor Mendonça <57025570+claudiovm2001@users.noreply.github.com>
2022-02-03 21:54:57 -0300

    Add comments

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
+    
+    #Código autoral abaixo:
     path("wiki/<str:title>", views.entry, name="entry"),
     path("create", views.create, name="create"),
     path("search", views.search, name="search"),
